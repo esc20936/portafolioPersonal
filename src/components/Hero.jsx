b9d6b2dc4eb285635ffaf9af7434f356ea10a1e3
esc20936 <esc20936@uvg.edu.gt>
2022-05-28 10:48:37 -0700

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
 
       <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "#120d21" }}>
         <div className="navbar-brand">
-          <img src={LogoImg} width="60" height="60" className="logoImg" alt="" />
+          {/* <img src={LogoImg} width="60" height="60" className="logoImg" alt="" /> */}
         </div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
@@ -39,7 +39,17 @@ export default function HeroSection() {
           <h1 className="title">INGENÍERO EN CIENCIAS DE LA COMPUTACIÓN Y TECNOLOGÍAS DE LA </h1>
           <span className="secondTitle">INFORMACIÓN</span>
         </div>
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
+        <div className="contact">
+          <div className="contactContainer">
+            <a className="social-icon" href="https://github.com/esc20936">
+              <i className="fa-brands fa-github icono"></i>
+            </a>
+            <a className="social-icon" href="https://www.linkedin.com/in/pablo-escobar-6188981a2/">
+              <i className="fa-brands fa-linkedin icono"></i>
+            </a>
+          </div>
+        </div>
+        {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" /> */}
         <div className="animationContainer">
           <Player
             autoplay
@@ -50,6 +60,20 @@ export default function HeroSection() {
         </div>
       </div>
 
+      <div className="sobreMi">
+      <div className="mainTitle sub">
+          <span className="title sub">Sobre mi</span>
+          <div className="aboutMeContainer">
+          <h3 className="description">
+            Soy estudiante de ciencias de la computación y tecnologías de información en la UVG.
+            Soy un apasionado de la tecnología, el cual disfruta aprender nuevas tecnologías. Mi enfoque va más orientado al desarrollo movil y al desarrollo web, con tecnologías tales como React, React Native y Xamarin.
+             
+          </h3>
+        </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
